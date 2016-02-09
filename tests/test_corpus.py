37bcccd843eb24305199ef0a2dc19737c0eec930
Dang Chuan Nguyen <nguyendc@systran.fr>
2016-02-09 11:29:38 +0100

    improve tests

--- tests/test_corpus.py
+++ tests/test_corpus.py
@@ -14,104 +14,130 @@ class CorpusApiTests(unittest.TestCase):
         self.api_client = systran_resources_api.ApiClient()
         self.corpus_api = systran_resources_api.CorpusApi(self.api_client)
 
-    def test_resources_corpus_list_get(self):
+    def test11_clean_existed_corpus(self):
+        corpus_ids = []
+        list_corpus = self.corpus_api.resources_corpus_list_get(prefix="pythonClientTest")
+        self.assertIsNotNone(list_corpus)
+        if list_corpus.files is not None:
+            for corpus in list_corpus.files:
+                if corpus.id is not None:
+                    corpus_ids.append(corpus.id)
+            if len(corpus_ids) > 0:
+                result = self.corpus_api.resources_corpus_delete_post(corpus_id=corpus_ids)
+                self.assertIsNotNone(result)
+                print result.__repr__()
+
+    def test21_resources_corpus_add_post(self):
+        lang = "en"
+        name = "pythonClientTestAddCorpus"
+        result = self.corpus_api.resources_corpus_add_post(name=name, lang=lang)
+        self.assertIsNotNone(result)
+        print result.__repr__()
+
+    def test22_resources_corpus_import_post(self):
+        input = "#TM\n" \
+                "#EN\tFR\n" \
+                "This is a test \t Ceci est un test"
+        format = "text/bitext"
+        name = "pythonClientTestImportCorpus"
+        result = self.corpus_api.resources_corpus_import_post(name=name, input=input, format=format)
+        self.assertIsNotNone(result)
+        print result.__repr__()
+
+    def test23_resources_corpus_import_post_with_file(self):
+        input_file = os.path.join(os.path.dirname(__file__), "", "corpus.txt")
+        format = "text/bitext"
+        name = "pythonClientTestFileImport"
+        result = self.corpus_api.resources_corpus_import_post(name=name, input_file=input_file, format=format)
+        self.assertIsNotNone(result)
+        print result.__repr__()
+
+    def get_corpus_id_by_prefix(self, prefix):
+        if prefix is None:
+            prefix = "pythonClientTest"
+        result = self.corpus_api.resources_corpus_list_get(prefix=prefix)
+        if result.files is not None and result.files[0].id is not None:
+            return result.files[0].id
+
+    def test12_resources_corpus_list_get(self):
         result = self.corpus_api.resources_corpus_list_get()
         self.assertIsNotNone(result)
         print result.__repr__()
 
-    def test_resources_corpus_list_get_with_source_target(self):
+    def test13_resources_corpus_list_get_with_source_target(self):
         source = "en"
         target = "fr"
         result = self.corpus_api.resources_corpus_list_get(source_lang=source, target_lang=target)
         self.assertIsNotNone(result)
         print result.__repr__()
 
-    def test_resources_corpus_list_get_with_prefix(self):
-        prefix = "testImport"
+    def test31_resources_corpus_list_get_with_prefix(self):
+        prefix = "pythonClientTest"
         result = self.corpus_api.resources_corpus_list_get(prefix=prefix)
-        self.assertIsNotNone(result)
         print result.__repr__()
 
-    def test_resources_corpus_add_post(self):
-        lang = "en"
-        name = "testAddCorpus"
-        print 'Enter a new corpus name and uncomment below codes to test'
-        # result = self.corpus_api.resources_corpus_add_post(name=name, lang=lang)
-        # self.assertIsNotNone(result)
-        # print result.__repr__()
-
-    def test_resources_corpus_import_post(self):
-        input = "#TM\n" \
-                "#EN\tFR\n" \
-                "This is a test \t Ceci est un test"
-        format= "text/bitext"
-        name = "testImport"
-        print 'Enter a new corpus name and uncomment below codes to test'
-        # result = self.corpus_api.resources_corpus_import_post(name=name, input=input, format=format)
-        # self.assertIsNotNone(result)
-        # print result.__repr__()
-
-    def test_resources_corpus_import_post_with_file(self):
-        input_file = os.path.join(os.path.dirname(__file__), "", "corpus.txt")
-        format = "text/bitext"
-        name = "testFileImport"
-        print 'Enter a new corpus name and uncomment below codes to test'
-        # result = self.corpus_api.resources_corpus_import_post(name=name, input_file=input_file, format=format)
-        # self.assertIsNotNone(result)
-        # print result.__repr__()
-
-    def test_resources_corpus_exists_get(self):
-        name = "testImport"
+    def test32_resources_corpus_exists_get(self):
+        name = "pythonClientTestAddCorpus"
         result = self.corpus_api.resources_corpus_exists_get(name=name)
         self.assertIsNotNone(result)
         print result.__repr__()
 
-    def test_resources_corpus_details_get(self):
-        corpus_id = "55b62288158ad061c6aaa509"
-        print 'Use a valid "corpus_id" and uncomment below codes to test'
-        # result = self.corpus_api.resources_corpus_details_get(corpus_id=corpus_id)
-        # self.assertIsNotNone(result)
-        # print result.__repr__()
+    def test33_resources_corpus_details_get(self):
+        corpus_id = self.get_corpus_id_by_prefix(prefix="pythonClientTest")
+        if corpus_id is None:
+            print "No corpus id found to get detail information"
+            return
+        result = self.corpus_api.resources_corpus_details_get(corpus_id=corpus_id)
+        self.assertIsNotNone(result)
+        print result.__repr__()
 
-    def test_resources_corpus_export_get(self):
-        corpus_id = "55b62288158ad061c6aaa509"
-        print 'Use a valid "corpus_id" and uncomment below codes to test'
-        # result = self.corpus_api.resources_corpus_export_get(corpus_id=corpus_id)
-        # self.assertIsNotNone(result)
-        # output_file = os.path.join(os.path.dirname(__file__), "", "exported_corpus.xml")
-        # fo = open(output_file, "wb")
-        # fo.write(result)
-        # fo.close()
-        # print result
+    def test34_resources_corpus_export_get(self):
+        corpus_id = self.get_corpus_id_by_prefix(prefix="pythonClientTest")
+        if corpus_id is None:
+            print "No corpus id found to export data"
+            return
+        result = self.corpus_api.resources_corpus_export_get(corpus_id=corpus_id)
+        self.assertIsNotNone(result)
+        output_file = os.path.join(os.path.dirname(__file__), "", "exported_corpus.xml")
+        fo = open(output_file, "wb")
+        fo.write(result)
+        fo.close()
+        print result
 
-    def test_resources_corpus_export_get_with_format(self):
-        corpus_id = "55b62288158ad061c6aaa509"
+    def test35_resources_corpus_export_get_with_format(self):
+        corpus_id = self.get_corpus_id_by_prefix(prefix="pythonClientTest")
+        if corpus_id is None:
+            print "No corpus id found to export data"
+            return
         format = "text/bitext"
-        print 'Use a valid "corpus_id" and uncomment below codes to test'
-        # result = self.corpus_api.resources_corpus_export_get(corpus_id=corpus_id, format=format)
-        # self.assertIsNotNone(result)
-        # output_file = os.path.join(os.path.dirname(__file__), "", "exported_corpus.txt")
-        # fo = open(output_file, "wb")
-        # fo.write(result)
-        # fo.close()
-        # print result
+        result = self.corpus_api.resources_corpus_export_get(corpus_id=corpus_id, format=format)
+        self.assertIsNotNone(result)
+        output_file = os.path.join(os.path.dirname(__file__), "", "exported_corpus.txt")
+        fo = open(output_file, "wb")
+        fo.write(result)
+        fo.close()
+        print result
 
-    def test_resources_corpus_update_post(self):
-        corpus_id = "55b62288158ad061c6aaa509"
-        name = "testImportPythonClient"
-        print 'Use a valid "corpus_id" and uncomment below codes to test'
-        # result = self.corpus_api.resources_corpus_update_post(corpus_id=corpus_id, name=name)
-        # self.assertIsNotNone(result)
-        # print result.__repr__()
+    def test36_resources_corpus_update_post(self):
+        corpus_id = self.get_corpus_id_by_prefix(prefix="pythonClientTest")
+        if corpus_id is None:
+            print "No corpus id found to update data"
+            return
+        name = "pythonClientTestUpdatedName"
+        result = self.corpus_api.resources_corpus_update_post(corpus_id=corpus_id, name=name)
+        self.assertIsNotNone(result)
+        print result.__repr__()
 
-    def test_resources_corpus_segment_list_get(self):
-        corpus_id = "55b62288158ad061c6aaa509"
-        print 'Use a valid "corpus_id" and uncomment below codes to test'
-        # result = self.corpus_api.resources_corpus_segment_list_get(corpus_id=corpus_id)
-        # self.assertIsNotNone(result)
-        # print result.__repr__()
+    def test41_resources_corpus_segment_list_get(self):
+        corpus_id = self.get_corpus_id_by_prefix(prefix="pythonClientTest")
+        if corpus_id is None:
+            print "No corpus id found to get segment list"
+            return
+        result = self.corpus_api.resources_corpus_segment_list_get(corpus_id=corpus_id)
+        self.assertIsNotNone(result)
+        print result.__repr__()
 
-    def test_resources_corpus_segment_add_post(self):
+    def test42_resources_corpus_segment_add_post(self):
         segment_target = systran_resources_api.CorpusAddSegmentTarget()
         segment_target.lang = "fr"
         segment_target.target = "Ceci est une belle maison"
@@ -124,38 +150,58 @@ class CorpusApiTests(unittest.TestCase):
         list_corpus_add_segments = [corpus_add_segment]
 
         corpus_segment_add_request = systran_resources_api.CorpusSegmentAddRequest()
-        corpus_segment_add_request.corpus_id = "55b62288158ad061c6aaa509"
+        corpus_segment_add_request.corpus_id = self.get_corpus_id_by_prefix(prefix="pythonClientTest")
+        if corpus_segment_add_request.corpus_id is None:
+            print "No corpus id found to add a segment"
+            return
         corpus_segment_add_request.segments = list_corpus_add_segments
 
-        print 'Use a valid "corpus_id" and uncomment below codes to test'
-        # result = self.corpus_api.resources_corpus_segment_add_post(body=corpus_segment_add_request)
-        # self.assertIsNotNone(result)
-        # print result.__repr__()
+        result = self.corpus_api.resources_corpus_segment_add_post(body=corpus_segment_add_request)
+        self.assertIsNotNone(result)
+        print result.__repr__()
 
-    def test_resources_corpus_segment_delete_post(self):
-        corpus_id = "55b62288158ad061c6aaa509"
-        seg_id = ["55b62288158ad061c6aaa508.55c0dda6fd400171d6f8428d"]
-        print 'Use a valid "corpus_id", "seg_id" and uncomment below codes to test'
-        # result = self.corpus_api.resources_corpus_segment_delete_post(corpus_id=corpus_id, seg_id=seg_id)
-        # self.assertIsNotNone(result)
-        # print result.__repr__()
 
-    def test_resources_corpus_delete_get(self):
-        corpus_ids = ["55b6311d158ad061c6aaa519", "55b633c4158ad061c6aaa51b"]
-        print 'Use a valid list of "corpus_ids" and uncomment below codes to test'
-        # result = self.corpus_api.resources_corpus_delete_post(corpus_id=corpus_ids)
-        # self.assertIsNotNone(result)
-        # print result.__repr__()
-
-    def test_resources_corpus_match_get(self):
-        corpus_ids = ["55b62288158ad061c6aaa509"]
+    def test51_resources_corpus_match_get(self):
+        corpus_ids = [self.get_corpus_id_by_prefix(prefix="pythonClientTest")]
+        if corpus_ids[0] is None:
+            print "No corpus id found for match operation"
+            return
         source = "en"
         target = "fr"
         inputs = ["This is a test", "This is a beautiful house"]
-        print 'Use a valid list of "corpus_ids" and uncomment below codes to test'
-        # result = self.corpus_api.resources_corpus_match_get(corpus_id=corpus_ids, input=inputs, source_lang=source, target_lang=target)
-        # self.assertIsNotNone(result)
-        # print result.__repr__()
+        result = self.corpus_api.resources_corpus_match_get(corpus_id=corpus_ids, input=inputs, source_lang=source, target_lang=target)
+        self.assertIsNotNone(result)
+        print result.__repr__()
+
+    def test61_resources_corpus_segment_delete_post(self):
+        corpus_id = self.get_corpus_id_by_prefix(prefix="pythonClientTest")
+        if corpus_id is None:
+            print "No corpus id found to delete a segment"
+            return
+        segment_list = self.corpus_api.resources_corpus_segment_list_get(corpus_id=corpus_id)
+        if segment_list.segments is None or segment_list.segments[0].id:
+            print "No segment id found to delete"
+            return
+        seg_id = [segment_list.segments[0].id]
+        result = self.corpus_api.resources_corpus_segment_delete_post(corpus_id=corpus_id, seg_id=seg_id)
+        self.assertIsNotNone(result)
+        print result.__repr__()
+
+    def test62_resources_corpus_delete_get(self):
+        corpus_ids = []
+        list_corpus = self.corpus_api.resources_corpus_list_get(prefix="pythonClientTest")
+        if list_corpus.files is None:
+            print "No corpus id found to delete"
+            return
+        for corpus in list_corpus.files:
+            if corpus.id is not None:
+                corpus_ids.append(corpus.id)
+        if len(corpus_ids) == 0:
+            print "No corpus id found to delete"
+            return
+        result = self.corpus_api.resources_corpus_delete_post(corpus_id=corpus_ids)
+        self.assertIsNotNone(result)
+        print result.__repr__()
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_dictionary.py
+++ tests/test_dictionary.py
@@ -14,12 +14,23 @@ class DictionaryApiTests(unittest.TestCase):
         self.api_client = systran_resources_api.ApiClient()
         self.dictionary_api = systran_resources_api.DictionaryApi(self.api_client)
 
-    def test_resources_lookup_supported_languages_get(self):
+    def get_dictionary_id(self):
+        match_filter = systran_resources_api.DictionariesListMatchFilter()
+        match_filter.regex_name = "pythonClientTest"
+
+        list_filters = systran_resources_api.DictionariesListFilters()
+        list_filters.match = match_filter
+
+        result = self.dictionary_api.resources_dictionary_list_post(filters=list_filters)
+        if result.dictionaries is not None and result.dictionaries[0].id is not None:
+            return result.dictionaries[0].id
+
+    def test11_resources_lookup_supported_languages_get(self):
         result = self.dictionary_api.resources_dictionary_lookup_supported_languages_get()
         self.assertIsNotNone(result)
         print result.__repr__()
 
-    def test_resources_lookup_get(self):
+    def test12_resources_lookup_get(self):
         source = "en"
         target = "fr"
         inputs = ["test", "work"]
@@ -27,7 +38,7 @@ class DictionaryApiTests(unittest.TestCase):
         self.assertIsNotNone(result)
         print result.__repr__()
 
-    def test_resources_lookup_get_with_auto_complete(self):
+    def test13_resources_lookup_get_with_auto_complete(self):
         source = "en"
         target = "fr"
         inputs = ["workaroun"]
@@ -35,18 +46,18 @@ class DictionaryApiTests(unittest.TestCase):
         self.assertIsNotNone(result)
         print result.__repr__()
 
-    def test_resources_dictionary_list_post(self):
+    def test14_resources_dictionary_list_post(self):
         result = self.dictionary_api.resources_dictionary_list_post()
         self.assertIsNotNone(result)
         print result.__repr__()
 
-    def test_resources_dictionary_add_post(self):
+    def test15_resources_dictionary_add_post(self):
         dict_add_input = systran_resources_api.DictionaryAddInput()
-        dict_add_input.name = "testPythonClient"
+        dict_add_input.name = "pythonClientTest"
         dict_add_input.source_lang = "en"
         dict_add_input.target_langs = "fr"
         dict_add_input.type = "UD"
-        dict_add_input.comments = "This is a dictionary created for client java test"
+        dict_add_input.comments = "This is a dictionary created for python client testing purposes"
 
         dict_add_body = systran_resources_api.DictionaryAddBody()
         dict_add_body.dictionary = dict_add_input
@@ -55,9 +66,9 @@ class DictionaryApiTests(unittest.TestCase):
         self.assertIsNotNone(result)
         print result.__repr__()
 
-    def test_resources_dictionary_list_post_with_match_filter(self):
+    def test16_resources_dictionary_list_post_with_match_filter(self):
         match_filter = systran_resources_api.DictionariesListMatchFilter()
-        match_filter.regex_name = "test"
+        match_filter.regex_name = "pythonClientTest"
 
         list_filters = systran_resources_api.DictionariesListFilters()
         list_filters.match = match_filter
@@ -66,7 +77,7 @@ class DictionaryApiTests(unittest.TestCase):
         self.assertIsNotNone(result)
         print result.__repr__()
 
-    def test_resources_dictionary_list_post_with_sort_filter(self):
+    def test17_resources_dictionary_list_post_with_sort_filter(self):
         sort_filter = systran_resources_api.DictionariesListSortFilter()
         sort_filter.name = 1    # sort by dictionary name in "ascending" order
 
@@ -77,31 +88,29 @@ class DictionaryApiTests(unittest.TestCase):
         self.assertIsNotNone(result)
         print result.__repr__()
 
-    def test_resources_dictionary_update_post(self):
-        dictionary_id = "55c0a88275314374575ae4d4"
+    def test21_resources_dictionary_update_post(self):
+        dictionary_id = self.get_dictionary_id()
+        if dictionary_id is None:
+            print "No dictionary found to update"
+            return
         dict_update_input = systran_resources_api.DictionaryUpdateInput()
-        dict_update_input.name = "testPythonClient"
+        dict_update_input.name = "pythonClientTestUpdatedName"
         dict_update_input.source_lang = "en"
         dict_update_input.target_langs = "fr"
-        dict_update_input.comments = "This dictionary has been updated by python client test"
+        dict_update_input.comments = "This dictionary has been updated by python client"
 
         dict_update_body = systran_resources_api.DictionaryUpdateBody()
         dict_update_body.dictionary = dict_update_input
 
-        print 'Use a valid "dictionary_id" and uncomment below codes to test'
-        # result = self.dictionary_api.resources_dictionary_update_post(dictionary_id=dictionary_id, input=dict_update_body)
-        # self.assertIsNotNone(result)
-        # print result.__repr__()
+        result = self.dictionary_api.resources_dictionary_update_post(dictionary_id=dictionary_id, input=dict_update_body)
+        self.assertIsNotNone(result)
+        print result.__repr__()
 
-    def test_resources_dictionary_delete_post(self):
-        dictionary_id = "55db168775314374575ae630"
-        print 'Use a valid "dictionary_id" and uncomment below codes to test'
-        # result = self.dictionary_api.resources_dictionary_delete_post(dictionary_id=dictionary_id)
-        # self.assertIsNotNone(result)
-        # print result.__repr__()
-
-    def test_resources_dictionary_entry_add_post(self):
-        dictionary_id = "5603ba796b5e3fa187e83604"
+    def test22_resources_dictionary_entry_add_post(self):
+        dictionary_id = self.get_dictionary_id()
+        if dictionary_id is None:
+            print "No dictionary found to add an entry"
+            return
         entry_add_input = systran_resources_api.EntryAddInput()
         entry_add_input.source_lang = "en"
         entry_add_input.target_lang = "fr"
@@ -111,17 +120,78 @@ class DictionaryApiTests(unittest.TestCase):
         entry_add_body = systran_resources_api.EntryAddBody()
         entry_add_body.entry = entry_add_input
 
-        print 'Use a valid "dictionary_id" and correspond "source lang", "target lang" then uncomment below codes to test'
-        # result = self.dictionary_api.resources_dictionary_entry_add_post(dictionary_id=dictionary_id, input=entry_add_body)
-        # self.assertIsNotNone(result)
-        # print result.__repr__()
+        result = self.dictionary_api.resources_dictionary_entry_add_post(dictionary_id=dictionary_id, input=entry_add_body)
+        self.assertIsNotNone(result)
+        print result.__repr__()
 
-    def test_resources_dictionary_entry_list_post(self):
-        dictionary_id = "55c0a88275314374575ae4d4"
-        print 'Use a valid "dictionary_id" and uncomment below codes to test'
-        # result = self.dictionary_api.resources_dictionary_entry_list_post(dictionary_id=dictionary_id)
-        # self.assertIsNotNone(result)
-        # print result.__repr__()
+    def test23_resources_dictionary_entry_list_post(self):
+        dictionary_id = self.get_dictionary_id()
+        if dictionary_id is None:
+            print "No dictionary found to get the list of entry"
+            return
+        result = self.dictionary_api.resources_dictionary_entry_list_post(dictionary_id=dictionary_id)
+        self.assertIsNotNone(result)
+        print result.__repr__()
+
+    def test24_resources_dictionary_entry_update_post(self):
+        dictionary_id = self.get_dictionary_id()
+        if dictionary_id is None:
+            print "No dictionary found to update an entry"
+            return
+        entry_update_input = systran_resources_api.EntryUpdateInput()
+        entry_update_input.source_lang = "en"
+        entry_update_input.target_lang = "fr"
+        entry_update_input.source = "house"
+        entry_update_input.target = "batiment"
+
+        entry_list = self.dictionary_api.resources_dictionary_entry_list_post(dictionary_id=dictionary_id)
+        if entry_list is None or entry_list.entries is None or entry_list.entries[0].source_id is None:
+            print "No entry found to update"
+            return
+        entry_update_body = systran_resources_api.EntryUpdateBody()
+        entry_update_body.source_id = entry_list.entries[0].source_id
+        entry_update_body.target_id = entry_list.entries[0].target_id
+        entry_update_body.update = entry_update_input
+
+        result = self.dictionary_api.resources_dictionary_entry_update_post(dictionary_id=dictionary_id, input=entry_update_body)
+        self.assertIsNotNone(result)
+        print result.__repr__()
+
+    def test31_resources_dictionary_entry_delete_post(self):
+        dictionary_id = self.get_dictionary_id()
+        if dictionary_id is None:
+            print "No dictionary found to delete an entry"
+            return
+        entry_list = self.dictionary_api.resources_dictionary_entry_list_post(dictionary_id=dictionary_id)
+        if entry_list.entries is None or entry_list.entries[0].source_id is None:
+            print "No entry found to delete"
+            return
+        entry_delete_input = systran_resources_api.EntryDeleteInput()
+        entry_delete_input.source_id = entry_list.entries[0].source_id
+        entry_delete_input.target_id = entry_list.entries[0].target_id
+
+        entry_delete_body = systran_resources_api.EntryDeleteBody()
+        entry_delete_body.entry = entry_delete_input
+
+        result = self.dictionary_api.resources_dictionary_entry_delete_post(dictionary_id=dictionary_id, input=entry_delete_body)
+        self.assertIsNotNone(result)
+        print result.__repr__()
+
+    def test32_resources_dictionary_delete_post(self):
+        match_filter = systran_resources_api.DictionariesListMatchFilter()
+        match_filter.regex_name = "pythonClientTest"
+
+        list_filters = systran_resources_api.DictionariesListFilters()
+        list_filters.match = match_filter
+
+        result = self.dictionary_api.resources_dictionary_list_post(filters=list_filters)
+        if result.dictionaries is None or len(result.dictionaries) == 0:
+            print "No dictionary found to delete"
+            return
+        for dictionary in result.dictionaries:
+            if dictionary.id is not None:
+                self.dictionary_api.resources_dictionary_delete_post(dictionary_id=dictionary.id)
+                print "Dictionary " + dictionary.id + " has been deleted"
 
 if __name__ == '__main__':
     unittest.main()
